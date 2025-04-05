22cac9a167267495e0654eca081c30a14d6b7e4c
SpreadSheets600 <sohammaity006@gmail.com>
2025-04-05 18:28:41 +0000

    Added new accountability

--- bot.py
+++ bot.py
@@ -148,7 +148,7 @@ try:
     bot.load_extension("handlers.reaction")
     bot.load_extension("utilities.feedback")
     # bot.load_extension("handlers.moderation")
-    bot.load_extension("utilities.accountability")
+    bot.load_extension("utilities.accountability.accountability")
 except Exception as e:
     print(f"Error Loading : {e}")
     traceback.print_exc()
